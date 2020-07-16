c43351f544b2f8f85690a715f3c282b1bc3396df
sabbir-ahmed007 <jhonplayer08>
2020-07-16 15:09:40 +0600

    Added Tupple

---
+++ tupple.py
@@ -0,0 +1,10 @@
+restaurent_buffet_menu= ('Soups','Fish','Meats','Chicken','Pasta')
+print("Orginal Menu")
+for item in restaurent_buffet_menu:
+    print(item)
+
+restaurent_buffet_menu= ('Seasonal Vegetables','Fish','Meats','Chicken','Pre-prepared salad')
+print("Revised Menu")
+for item in restaurent_buffet_menu:
+    print(item)
+    
